5004e253a6c0b0d029f5eb15d427fe9fd0bacd45
Marcos Cunha <marcos.cunha@roxcode.dev>
2022-12-28 16:29:53 -0300

    mudando paginas

--- lib/templates.dart
+++ lib/templates.dart
@@ -15,7 +15,36 @@ class [PAGE_NAME]Binding implements Bindings {
 const String controllerTemplate = """
 import 'package:get/get.dart';
 
-class [PAGE_NAME]Controller extends GetxController {}
+class [PAGE_NAME]Controller extends GetxController with LoadingMixin {
+
+  @override
+  void onInit(){
+    super.onInit();
+    if(loadParameters()){
+      fetchData(showLoading: true);
+    }
+  }
+
+  bool loadParameters(){
+    try {
+      // parameter1 = Get.find<Type>('parameter1');
+      // parameter2 = Get.find<Type>('parameter2');
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  void fetchData({bool showLoading=false}){
+    if(showLoading){
+      showLoadingWhileRunning(() => _fetchData());
+    }else{
+      _fetchData();
+    }
+  }
+
+  Future _fetchData() async {}
+}
 """;
 
 const String pageTemplate = """
@@ -26,6 +55,14 @@ import '../controllers/[PAGE_NAME_SNAKE]_controller.dart';
 class [PAGE_NAME]Page extends GetView<[PAGE_NAME]Controller>{
 
   const [PAGE_NAME]Page({super.key});
+
+  static Future to() async {
+    // Get.delete<Type>(tag: 'parameter1');
+    // Get.delete<Type>(tag: 'parameter2');
+    
+    // Get.put<Type>(parameter1, tag: 'parameter1');
+    // Get.put<Type>(parameter2, tag: 'parameter2');
+  }
 
   @override
   Widget build(BuildContext context) {
